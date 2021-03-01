28ba87409bc8188c7854ea323aff973ec2a76397
wngur6076 <wngur6076@naver.com>
2021-03-01 17:56:04 +0900

    09 - Testing-Driving the Real Stripe Adapter

---
+++ app/Billing/StripePaymentGateway.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Billing;
+
+use Stripe\Charge;
+use Stripe\Exception\InvalidRequestException;
+
+class StripePaymentGateway implements PaymentGateway
+{
+    private $apiKey;
+
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function charge($amount, $token)
+    {
+        try {
+            Charge::create([
+                'amount' => $amount,
+                'source' => $token,
+                'currency' => 'usd',
+            ], ['api_key' => $this->apiKey]);
+        } catch (InvalidRequestException $e) {
+            throw new PaymentFailedException;
+        }
+
+        /* (new \GuzzleHttp\Client)->post('https://api.stripe.com/v1/charges', [
+            'headers' => [
+                'Authorization' => "Bearer {$this->apiKey}",
+            ],
+            'form_params' => [
+                'amount' => $amount,
+                'source' => $token,
+                'currency' => 'usd',
+            ]
+        ]); */
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
+use App\Models\Concert;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Concert::factory()->published()->create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'laraville',
+            'state' => 'ON',
+            'zip' => "17916",
+            'date' => Carbon::parse('2021-12-13 8:00pm'),
+            'ticket_price' => 3250,
+            'additional_information' => 'This concert is 19+.',
+        ])->addTickets(10);
     }
 }
